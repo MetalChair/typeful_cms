14b18f34de927eacc31a09ab4a3bc736d52d5c2e
Logan <logan.scott441@gmail.com>
2022-01-27 18:38:46 -0500

    Further work on v1.0

--- typeful_cms_server/tests/integration/test_rows.py
+++ typeful_cms_server/tests/integration/test_rows.py
@@ -3,7 +3,7 @@ import test_models
 def test_create_row(test_app : AppContext):
     test_models.test_simple_model_creation(test_app)
     res = test_app.app.test_client().get(
-        "/Users?name=Leanne Graham&includes=Address,Company"
+        "/Users?name=Leanne Graham&includes=Address[Geo],Company"
     )
     res_json = res.get_json()
     return
